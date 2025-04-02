82f53fbf2a04258092eb204a16908e2da758672d
enastaher55 <enastaher06@gmail.com>
2025-04-02 21:29:17 +0200

    removed verify otp screen, added complete user data in case of sign in with google and finished auth logic

--- lib/screens/auth/logic/cubit/auth_state.dart
+++ lib/screens/auth/logic/cubit/auth_state.dart
@@ -30,3 +30,30 @@ final class LoginFailed extends LoginState {
 
   LoginFailed(this.error);
 }
+
+final class LogoutState extends AuthState {}
+final class LogoutLoading extends LogoutState {}
+final class LogoutSuccess extends LogoutState {}
+final class LogoutFailed extends LogoutState {
+  final String error;
+
+  LogoutFailed(this.error);
+}
+
+final class GoogleLoginState extends AuthState {}
+final class GoogleLoginLoading extends GoogleLoginState {}
+final class GoogleLoginSuccess extends GoogleLoginState {}
+final class GoogleLoginFailed extends GoogleLoginState {
+  final String error;
+
+  GoogleLoginFailed(this.error);
+}
+
+
+final class ProfileUpdateState extends AuthState {}
+final class ProfileUpdateLoading extends ProfileUpdateState {}
+final class ProfileUpdateSuccess extends ProfileUpdateState {}
+final class ProfileUpdateFailed extends ProfileUpdateState {
+  final String error;
+  ProfileUpdateFailed(this.error);
+}
